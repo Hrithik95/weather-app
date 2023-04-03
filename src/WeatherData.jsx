63615e1491d24b4637ec0b2a5ab551a7e62f39f9
Hrithik Singh <hrithiksingh291@gmail.com>
2023-04-03 22:37:26 +0530

    first commit

---
+++ src/WeatherData.jsx
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+
+const WeatherData = () => {
+  const [search, setSearch] = useState("delhi");
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState("");
+  let componentMounted = true;
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=811f23709d4da521a86fcafe08b5e842`
+      );
+      if (componentMounted) {
+        setData(await response.json());
+        console.log(data);
+        console.log(data.name);
+      }
+      return () => {
+        componentMounted = false;
+      };
+    };
+    fetchWeather();
+  }, [search]);
+
+  let emoji = null;
+  if (data.main) {
+    if (data.weather[0].main === "Clouds") {
+      emoji = "fa-cloud";
+    } else if (data.weather[0].main === "Tunderstorm") {
+      emoji = "fa-bolt";
+    } else if (data.weather[0].main === "Drizzle") {
+      emoji = "fa-cloud-rain";
+    } else if (data.weather[0].main === "Rain") {
+      emoji = "fa-cloud-shower-heavy";
+    } else if (data.weather[0].main === "Snow") {
+      emoji = "fa-snow-flake";
+    } else {
+      emoji = "fa-smog";
+    }
+  } else {
+    return <h1 className="loading">Loading...</h1>;
+  }
+
+  let temp = (data.main.temp - 273.15).toFixed(2);
+  let temp_max = (data.main.temp_max - 273.15).toFixed(2);
+  let temp_min = (data.main.temp_min - 273.15).toFixed(2);
+  let temp_feels_like = (data.main.feels_like - 273.15).toFixed(2);
+
+  let d = new Date();
+  let date = d.getDate();
+  let year = d.getFullYear();
+  let month = d.toLocaleString("default", { month: "long" });
+  let day = d.toLocaleString("default", { weekday: "long" });
+
+  let time = d.toLocaleString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(input);
+  };
+
+  return (
+    <div>
+      <div className="container mt-5">
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div className="card text-white text-center border-0">
+              <img
+                src={`https://source.unsplash.com/600x900/?${data.weather[0].main}`}
+                className="card-img"
+                alt="bgimg"
+              />
+              <div className="card-img-overlay">
+                <form onSubmit={handleSubmit}>
+                  <div className="input-group mb-3 w-75 mx-auto">
+                    <input
+                      type="search"
+                      className="form-control"
+                      placeholder="Search Your City Here..."
+                      aria-label="Search City"
+                      aria-describedby="basic-addon2"
+                      name="search"
+                      value={input}
+                      onChange={(event) => setInput(event.target.value)}
+                      required
+                    />
+                    <button type="submit">Submit</button>
+                  </div>
+                </form>
+                <div className="bg-dark bg-opacity-50 py-3 rounded">
+                  <h2 className="card-title">{data.name}</h2>
+                  <p className="card-text lead">
+                    {day}, {month} {date}, {year}
+                    <br />
+                    {time}
+                  </p>
+
+                  <hr />
+                  <i className={`fas ${emoji} fa-4x`}></i>
+                  <h1 className="fw-bolder mb-3">{temp} &deg;C</h1>
+                  <p className="lead fw-bolder mb-0">Feels Like</p>
+                  <p className="lead">{temp_feels_like}&deg;C</p>
+                  <p className="lead fw-bolder mb-0">{data.weather[0].main}</p>
+                  <p className="lead">
+                    {temp_max}&deg;C | {temp_min}&deg;C
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherData;
